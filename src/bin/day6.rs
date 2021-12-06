6c350117d088e468bf963392089df0b165ba56cd
Lucas Malandrino <lucas.malandrino@gmail.com>
2021-12-06 21:04:06 +0100

    Day 6 but less retarded

--- src/bin/day6.rs
+++ src/bin/day6.rs
@@ -1,43 +1,35 @@
 use aoc2021::answer;
-use rayon::prelude::*;
 
 const INPUT: &str = include_str!("../../inputs/day6.txt");
 
-type Fish = i8;
+fn simulate(fishes: &[i32], days: usize) -> u64 {
+    let mut fishes = {
+        let mut tmp = [0; 9];
+        for &f in fishes {
+            tmp[f as usize] += 1;
+        }
+        tmp
+    };
 
-fn simulate(fishes: &[Fish], days: usize) -> usize {
-    let mut fishes = fishes.to_vec();
 
     for day in 0..days {
-        println!("Day {}, fishes to process: {}", day, fishes.len());
+        let prego = fishes[0];
+        for i in 1..=8 {
+            fishes[i-1] = fishes[i];
+        }
 
-        let to_spawn = fishes
-            .par_chunks_mut(1_048_576)
-            .map(|fishes| {
-                let mut to_spawn_local = 0;
-                for f in fishes {
-                    *f -= 1;
-                    if *f < 0 {
-                        *f = 6;
-                        to_spawn_local += 1;
-                    }
-                }
-                to_spawn_local
-            })
-            .sum();
-
-        fishes.reserve(to_spawn);
-        fishes.extend(std::iter::repeat(8).take(to_spawn));
+        fishes[6] += prego;
+        fishes[8] = prego;
     }
 
-    fishes.len()
+    fishes.iter().sum()
 }
 
-fn part1(fishes: &[Fish]) -> usize {
+fn part1(fishes: &[i32]) -> u64 {
     simulate(fishes, 80)
 }
 
-fn part2(fishes: &[Fish]) -> usize {
+fn part2(fishes: &[i32]) -> u64 {
     simulate(fishes, 256)
 }
 
@@ -46,8 +38,7 @@ fn main() {
         .trim()
         .split(",")
         .map(|f| f.parse().unwrap())
-        .collect::<Vec<Fish>>();
+        .collect::<Vec<i32>>();
 
-    // answer(6, part1(&fishes), part2(&fishes));
-    answer(6, "Skip", part2(&fishes));
+    answer(6, part1(&fishes), part2(&fishes));
 }
